2e902857e487cb9769ce58d6c8887c906863b484
Mark Hiner <hiner@wisc.edu>
2012-08-20 10:43:14 -0500

    Scifio translators preserve implicit Metadata
    The small amount of state that is tracked by Metadata (regarding how it
    was created - i.e., filtered, metadataoptions, and source) are now
    maintained through translation, as these properties are common to all
    Metadata implementations.
    
    Added protected setters in the AbstractMetadata layer to facilitate the
    preservation of the data, and the AbstractTranslator now automatically
    carries out the transfer.
    
    These changes also required that all AbstractTranslator extenders make a
    super.translate call.

--- components/scifio-devel/src/ome/scifio/apng/APNGFormat.java
+++ components/scifio-devel/src/ome/scifio/apng/APNGFormat.java
@@ -910,7 +910,10 @@ public class APNGFormat
     }
     
     // -- Translator API Methods -- 
+    
+    @Override
     public void translate(final CoreMetadata source, final Metadata dest) {
+      super.translate(source, dest);
   
       final APNGIHDRChunk ihdr =
         dest.getIhdr() == null ? new APNGIHDRChunk() : dest.getIhdr();
@@ -1008,7 +1011,9 @@ public class APNGFormat
   
     // -- Translator API Methods --
   
+    @Override
     public void translate(final Metadata source, final CoreMetadata dest) {
+      super.translate(source, dest);
       final CoreImageMetadata coreMeta = new CoreImageMetadata();
       dest.add(coreMeta);
   
