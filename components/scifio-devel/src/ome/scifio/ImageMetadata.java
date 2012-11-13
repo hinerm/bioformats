33c85d937efa41d52698c8e2f4e078c64bc59505
Mark Hiner <hinerm@gmail.com>
2012-11-13 14:12:49 -0600

    fix: Commented ImageMetadata

--- components/scifio-devel/src/ome/scifio/ImageMetadata.java
+++ components/scifio-devel/src/ome/scifio/ImageMetadata.java
@@ -40,87 +40,173 @@ import java.util.Hashtable;
 import net.imglib2.meta.AxisType;
 
 /**
- * TODO
+ * ImageMetadata stores the metadata for a single image
+ * within a dataset. Here, information common to every
+ * image type (e.g. height, width, index information)
+ * can be stored in a standard way.
  * 
  * @author Mark Hiner
  *
  */
 public interface ImageMetadata {
 
+  /** Sets width (in pixels) of thumbnail planes in this image. */
   void setThumbSizeX(int thumbSizeX);
 
+  /** Sets height (in pixels) of thumbnail planes in this image. */
   void setThumbSizeY(int thumbSizeY);
 
+  /**
+   * Sets the number of bytes per pixel.  Must be one of the <i>static</i>
+   * pixel types (e.g. <code>INT8</code>) in {@link ome.scifio.util.FormatTools}.
+   */
   void setPixelType(int pixelType);
 
+  /** Sets the number of valid bits per pixel. */
   void setBitsPerPixel(int bitsPerPixel);
 
+  /** Sets the length of each subdimension of C. */
   void setChannelLengths(int[] cLengths);
 
+  /** Sets the name of each subdimension of C. */
   void setChannelTypes(String[] cTypes);
 
+  /**
+   * Sets whether or not we are confident that the
+   * dimension order is correct.
+   */
   void setOrderCertain(boolean orderCertain);
 
+  /**
+   * Sets whether or not the planes are stored as RGB
+   * (multiple channels per plane).
+   */
   void setRgb(boolean rgb);
 
+  /** Sets whether or not each pixel's bytes are in little endian order. */
   void setLittleEndian(boolean littleEndian);
 
+  /**
+   * Set true if channels are stored RGBRGBRGB...; false if channels are stored
+   * RRR...GGG...BBB...
+   */
   void setInterleaved(boolean interleaved);
 
+  /** Sets whether or not the planes are stored as indexed color. */
   void setIndexed(boolean indexed);
 
+  /** Sets whether or not we can ignore the color map (if present). */
   void setFalseColor(boolean falseColor);
 
+  /**
+   * Sets whether or not we are confident that all of the metadata stored
+   * within the image has been parsed.
+   */
   void setMetadataComplete(boolean metadataComplete);
 
+  /** Sets a collection of non-core metadata associated with this image. */
   void setImageMetadata(Hashtable<String, Object> imageMetadata);
 
+  /** 
+   * Sets whether or not this image is a lower-resolution copy of
+   * another image.
+   */
   void setThumbnail(boolean thumbnail);
 
+  /** 
+   * Sets the Axes types for this image. 
+   * Order is implied by ordering within this array
+   */
   void setAxisTypes(AxisType[] axisTypes);
 
+  /** Sets the lengths of each axis. Order is parallel of axisTypes. */
   void setAxisLengths(int[] axisLengths);
 
+  /** 
+   * Sets the length for the specified axis, 
+   * if its type is present in the image. 
+   */
   void setAxisLength(AxisType axis, int length);
 
+  /** Sets the type of the axis at the specified index. */
   void setAxisType(int index, AxisType axis);
 
+  /** Sets the number of planes within this image. */
   void setPlaneCount(int planeCount);
 
+  /** Returns the number of planes within this image. */
   int getPlaneCount();
 
+  /** Returns the width (in pixles) of the thumbnail planes in this image. */
   int getThumbSizeX();
 
+  /** Returns the height (in pixles) of the thumbnail planes in this image. */
   int getThumbSizeY();
 
+  /** 
+   * Returns the number of bytes per pixel. Should correlate with the
+   * pixel types in {@link ome.scifio.util.FormatTools}
+   */
   int getPixelType();
 
+  /** Returns the number of valid bits per pixel. */
   int getBitsPerPixel();
 
+  /** Returns the length of each subdimension of C. */
   int[] getChannelLengths();
 
+  /** Returns the name of each subdimension of C. */
   String[] getChannelTypes();
 
+  /** 
+   * Returns the Axes types for this image. 
+   * Order is implied by ordering within this array.
+   */
   AxisType[] getAxisTypes();
 
+  /** Returns the lengths of each axis. Order is parallel of axisTypes. */
   int[] getAxisLengths();
 
+  /**
+   * Returns true if we are confident that the
+   * dimension order is correct.
+   */
   boolean isOrderCertain();
 
+  /**
+   * Returns true if the planes are stored as RGB
+   * (multiple channels per plane).
+   */
   boolean isRgb();
 
+  /** Returns true if each pixel's bytes are in little endian order. */
   boolean isLittleEndian();
 
+  /**
+   * Returns true if channels are stored RGBRGBRGB...; false if channels are stored
+   * RRR...GGG...BBB...
+   */
   boolean isInterleaved();
 
+  /** Returns true if the planes are stored as indexed color. */
   boolean isIndexed();
 
+  /** Returns true if we can ignore the color map (if present). */
   boolean isFalseColor();
 
+  /**
+   * Returns true if we are confident that all of the metadata stored
+   * within the image has been parsed.
+   */
   boolean isMetadataComplete();
 
+  /** Returns a collection of non-core metadata associated with this image. */
   Hashtable<String, Object> getImageMetadata();
 
+  /** 
+   * Returns whether or not this image is a lower-resolution copy of
+   * another image.
+   */
   boolean isThumbnail();
 
 }
